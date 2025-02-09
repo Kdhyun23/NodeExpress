25c2a17a2c6e32acc1b234afdc26b91374d83151
김대현 <jau0723@naver.com>
2025-02-09 20:30:08 +0900

    express 설치

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function(){
+    console.log('Server is running on port 8080');
+});
+
+app.get('/test', function(req, res){
+    res.send('Hello World');
+});
