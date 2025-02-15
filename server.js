efdce88bcac7bb16a21c9efbd7331013efdb81be
김대현 <jau0723@naver.com>
2025-02-15 23:06:46 +0900

    폴더경로 수정, index화면 불러오기

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const app = express();
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -6,8 +8,27 @@ const connection = mysql.createConnection({
     database: 'dbtest'
 });
 
-connection.connect();
+app.use(express.static('public'));
 
+app.listen(8080, function(){
+    console.log('Server is running on port 8080');
+});
+
+app.get('/index', function(req, res){
+    //res.send('npm run nodemon');
+    res.sendFile(__dirname + '/src/index.html');
+});
+
+app.get('/select', function(req, res){
+    connection.connect();
+    connection.query('SELECT * FROM userinfo', (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    });
+    connection.end();
+});
+
+connection.connect();
 connection.query('SELECT * FROM userinfo', (error, results) => {
     if (error) throw error;
 
